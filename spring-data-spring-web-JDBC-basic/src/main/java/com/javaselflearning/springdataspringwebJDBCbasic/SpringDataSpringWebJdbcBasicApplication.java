ec064d7b488a44941940af72385fb59cc9160527
shrikant Dande <shrikant.dande@hitachivantara.com>
2022-06-17 21:03:41 +0530

    Spring data JDBC template project

---
+++ spring-data-spring-web-JDBC-basic/src/main/java/com/javaselflearning/springdataspringwebJDBCbasic/SpringDataSpringWebJdbcBasicApplication.java
@@ -0,0 +1,13 @@
+package com.javaselflearning.springdataspringwebJDBCbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSpringWebJdbcBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSpringWebJdbcBasicApplication.class, args);
+	}
+
+}
---
+++ spring-data-spring-web-JDBC-basic/src/test/java/com/javaselflearning/springdataspringwebJDBCbasic/SpringDataSpringWebJdbcBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaselflearning.springdataspringwebJDBCbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSpringWebJdbcBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
